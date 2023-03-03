b9d870e9db2cd286371a7885b8551d5f3a417a0c
melon musk <125475444+JoaomBRosio@users.noreply.github.com>
2023-03-02 23:31:11 -0300

    Add files via upload

---
+++ aula_10/index.php
@@ -0,0 +1,45 @@
+<?php
+//operadores logicos
+
+$valor = 10;
+$valor2 = '10';
+
+//$nome = 'joao';
+//$nome2 = 'gui';
+
+//UM SINAL DE IGUAL QUER DIZER QUE ESTAMOS ATRIBUINDO UM VALOR 
+//SEMPRE RETORNA VERDADEIRO
+/*if($nome = $nome2){
+    echo 'verdade';
+}
+*/
+
+/*
+//APENAS CONFERE SE O VALOR É IGUAL OU NÃO
+if($nome == $nome2){
+    echo 'verdade';
+}
+
+//CONFERIMOS PARA VER SE É DIFERENTE 
+if($nome != $nome2){
+    echo 'é diferente';
+}
+*/
+
+/*
+//CONFERE SE SÃO DE MESMO VALOR E MESMO TIPO/identicos
+if($valor === $valor2){
+    echo "estes numeros: $valor e $valor2, são identicos";
+}else{
+    echo 'nao sao iguais';
+}
+*/
+
+//comparamos para ver se sao diferentes (tipo e valore)
+if($valor !== $valor2){
+    echo 'verdade';
+}
+
+
+
+?>
